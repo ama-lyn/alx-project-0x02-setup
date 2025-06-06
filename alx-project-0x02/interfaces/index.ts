4454f21d59d6fbc6355ae828e5f390f013fed10a
Gwendolyn <wilkistagwendolyn@gmail.com>
2025-06-06 20:41:35 +0300

    UserCard

--- alx-project-0x02/interfaces/index.ts
+++ alx-project-0x02/interfaces/index.ts
@@ -11,6 +11,35 @@ export interface PostProps {
   content: string;
 }
 
+export interface UserProps {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: number;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: number;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+export interface UserModalProps {
+  onClose: () => void;
+  onSubmit: (post: UserProps) => void;
+}
+
 
 export interface CardModalProps {
   onClose: () => void;

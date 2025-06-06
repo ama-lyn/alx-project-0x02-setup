d7bc35f6e46066cbb0c4019cf8547c83f174e52f
Gwendolyn <wilkistagwendolyn@gmail.com>
2025-06-06 15:23:43 +0300

    Project scaffold

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-2xl font-bold">Amanda</h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <Link href="/" className="hover:underline" />
+              Home
+            </li>
+            <li>
+              <Link href="/about" className="hover:underline" />
+              About
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ alx-project-0x02/pages/index.tsx
@@ -0,0 +1,14 @@
+import Header from '@/components/layout/Header';
+
+const Home = () => {
+return(
+  <div>
+    <Header />
+    <main className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Welcome to My Application</h1>
+    </main>
+  </div>
+)
+}
+
+export default Home;
